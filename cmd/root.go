d3fa445d2472bf3f3340015fd7eaed74518a993d
bodrovis <elskruk@proton.me>
2025-11-03 21:42:59 +0200

    bump

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/bodrovis/lokalise-glossary-guard/cmd/validate"
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 )
 
 var version = "dev"
@@ -30,5 +34,22 @@ and Y/N flags catching the most common issues (wrong delimiter, missing term/des
 		},
 	})
 
+	rootCmd.AddCommand(&cobra.Command{
+		Use:    "gendocs",
+		Hidden: true,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := generateDocs(rootCmd, "./docs"); err != nil {
+				fmt.Fprintf(os.Stderr, "error generating docs: %v", err)
+			}
+		},
+	})
+
 	return rootCmd
+}
+
+func generateDocs(rootCmd *cobra.Command, dir string) error {
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		return err
+	}
+	return doc.GenMarkdownTree(rootCmd, dir)
 }
